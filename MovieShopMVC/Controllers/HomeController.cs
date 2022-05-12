b8c198e7cf7d3c90de0d24da96997acc573da931
Therria <103522581+Therria@users.noreply.github.com>
2022-05-11 21:54:20 -0700

    Add files via upload

---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult topRatedMovies()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var moiveService = new MovieService();
+            var movieCards = moiveService.GetTop30GrossingMovies();
+            return View(movieCards);
+        }
+
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
