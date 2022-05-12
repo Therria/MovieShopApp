c907278864959d305bb49c2f99c7a270303c6305
Therria <103522581+Therria@users.noreply.github.com>
2022-05-11 21:58:16 -0700

    Add files via upload

---
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public List<Movie> GetTop30GrossingMovies()
+        {
+            // Dapper
+            // var top30Movies = movies.GetMovies().OrderByDescending(m => m.Revenue).Take(30);
+            throw new NotImplementedException();
+        }
+    }
+}
