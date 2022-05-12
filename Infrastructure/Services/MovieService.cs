0b6854b2380ebae034fa0c2db78c363d712fad52
Therria <103522581+Therria@users.noreply.github.com>
2022-05-11 21:58:30 -0700

    Add files via upload

---
+++ Infrastructure/Services/MovieService.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public List<MovieCardModel> GetTop30GrossingMovies()
+        {
+            var movieRepo = new MovieRepository();
+            var movies = movieRepo.GetTop30GrossingMovies();
+
+            var movieCards = new List<MovieCardModel>();
+            foreach (var movie in movies)
+            {
+                movieCards.Add( new MovieCardModel 
+                { 
+                    Id = movie.Id,
+                    Title = movie.Title,
+                    PosterUrl = movie.PosterUrl
+                });
+            }
+
+            return movieCards;
+        }
+    }
+}
